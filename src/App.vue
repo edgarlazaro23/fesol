a9152075cd8aecf7e7bc4a4349312268f6a5ef4a
Edgar Lazaro <edgar.lazaro@tismart.com>
2019-11-13 14:27:12 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <b-button-group>
+        <b-button>Button 1</b-button>
+        <b-button>Button 2</b-button>
+        <b-button>Button 3</b-button>
+      </b-button-group>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
