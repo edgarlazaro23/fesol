554747f4004809cbd08efc7c4b109f9692522b01
Edgar Lazaro <edgar.lazaro@tismart.com>
2020-01-06 11:05:54 -0500

    carrusel

--- src/components/shared/Carrusel.vue
+++ src/components/shared/Carrusel.vue
@@ -1,43 +1,36 @@
 <template lang="pug">
-    div
-        b-carousel#carousel-1(v-model='slide', :interval='4000', controls='', indicators='' background='#ababab', img-width='1024', img-height='480', style='text-shadow: 1px 1px 2px #333;', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
-            // Text slides with image
-            b-carousel-slide(caption='First slide', text='Nulla vitae elit libero, a pharetra augue mollis interdum.' img-src='https://picsum.photos/1024/480/?image=52')
-            // Slides with custom text
-            b-carousel-slide(img-src='https://picsum.photos/1024/480/?image=54')
-                h1 Hello world!
-            // Slides with image only
-            b-carousel-slide(img-src='https://picsum.photos/1024/480/?image=58')
-            // Slides with img slot
-            // Note the classes .d-block and .img-fluid to prevent browser default image alignment
-            b-carousel-slide
-                template(v-slot:img='')
-                    img.d-block.img-fluid.w-100(width='1024', height='480', src='https://picsum.photos/1024/480/?image=55', alt='image slot')
-            // Slide with blank fluid image to maintain slide aspect ratio
-            b-carousel-slide(caption='Blank Image', img-blank='', img-alt='Blank image')
-                p
-                    | Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-                    | a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        p.mt-4
-            | Slide #: {{ slide }}
-            br
-            | Sliding: {{ sliding }}
+	b-carousel#carousel-1(v-model='slide', :interval='4000', controls='', indicators='' background='#ababab', style='text-shadow: 1px 1px 2px #333;', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
+		div(v-for="(item, i) in imagenes", :key="i" )
+			b-carousel-slide(:img-src="item.src", :caption="item.titulo", :text='item.text')
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
-    },
-    methods: {
-      onSlideStart() {
-        this.sliding = true
-      },
-      onSlideEnd() {
-        this.sliding = false
-      }
-    }
-  }
+import imagenOne  from "../../assets/img/carrusel/1.png";
+import imagenTwo  from "../../assets/img/carrusel/2.png";
+import imagenTree from "../../assets/img/carrusel/3.png";
+import imagenFour  from "../../assets/img/carrusel/4.png";
+	export default {
+		data() {
+			return {
+				imagenes:[
+					{ src: imagenTwo, titulo: 'IMAGEN 1', text:'Primera imagen de prueba' },
+					{ src: imagenOne, titulo: 'IMAGEN 2', text:'Primera imagen de prueba' },
+					{ src: imagenTree, titulo: 'CAMARA DE ALMACENAMIENTO (LURÍN)', text:'Primera imagen de prueba' },
+					{ src: imagenFour, titulo: 'DHL', text:'Primera imagen de prueba' },
+				],
+					slide: 0,
+					sliding: null
+				}
+			},
+			methods: {
+				onSlideStart() {
+					this.sliding = true
+				},
+				onSlideEnd() {
+					this.sliding = false
+				}
+		}
+	}
 </script>
+<style lang="scss">
+
+</style>
